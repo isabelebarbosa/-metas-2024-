b5a647d89311d38963479815a78b4ea533f50b2b
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2024-03-18 10:31:08 -0300

    jkhh

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ for(let i=0;i <botoes.length;i++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        Text[i].classList.add("ativo")
     }
 }
 
+const contadores = document.querySelectorAll(".contador");
+const tempoOObjetivo1 = new Date('2024-12-31 T00:00:00');
+contadores[0].textContent = tempoOObjetivo1[]
 
+let
