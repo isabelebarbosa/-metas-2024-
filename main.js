014cbcbbc44edfccba9a42cdff1a8a0170defad3
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2024-03-18 10:19:36 -0300

    ;çll

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const botoes = dovument.queryselectorAll("botao");
-//console.log(botoes.length;i++){
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
-        for  (let j=0;j <botoes.length;j++){
+        for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
     }
-    conaole.log();
 }
+
+
