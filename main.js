ead06d439fc904b4de72d58888d8d30635e35fca
isabelebarbosa <105320072+isabelebarbosa@users.noreply.github.com>
2024-05-14 11:51:35 -0300

    g

--- main.js
+++ main.js
@@ -10,4 +10,19 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo")
     }
 }
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
 
